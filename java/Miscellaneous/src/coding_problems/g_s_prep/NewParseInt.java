65552b12e23e80e2f481f33a11b7d7924ba3bf42
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2020-01-12 12:25:59 -0500

    Added ability to parse negative numbers, added ability to parse strings entered with separator characters of comma or underscore, fixed catch of inappropriate characters, added comments and exception messages for strings that aren't integers and strings representing numbers that are too large, added to tests in main with commented out tests for exceptions

--- java/Miscellaneous/src/coding_problems/g_s_prep/NewParseInt.java
+++ java/Miscellaneous/src/coding_problems/g_s_prep/NewParseInt.java
@@ -7,40 +7,64 @@ public class NewParseInt {
 	//  Problem: implement your own version of parseInt()
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Should be 112 " + newParseInt("112"));
 		System.out.println("Should be 165412 + 1 : " + (newParseInt("165412") + 1));
+		System.out.println("Should be -25:  " + newParseInt("-25"));
+		/* System.out.println("Should throw exception with not an integer message" 
+				+ newParseInt("114k")); */
+		/* System.out.println("Should throw exception with number too large exception" 
+				+ newParseInt("10000000000")); */
+		System.out.println("Should be 125000:  " + newParseInt("125,000"));
+		System.out.println("Should be 15000:  " + newParseInt("15_000"));
 	}
 	
 	private static int newParseInt(String s) {
 		
+		// if first char is '-' flags number as negative
+		boolean isNegative = false;
+		if (s.charAt(0) == '-') {
+			isNegative = true;
+		}
+		
 		char[] cArr = s.toCharArray();
-		for (int i= 0; i < s.length(); i++) {
-			cArr[i] -= '0';
-			if (cArr[i] > 9) {
-				throw new NumberFormatException();
+		//if is negative, ignores first character '-'
+		int startIndex = isNegative ? 1 : 0;
+		for (int i = startIndex; i < s.length(); i++) {
+			// for non separator characters, changing char value to int equivalent of digit
+			if (cArr[i] != '_' && cArr[i] != ',') cArr[i] -= '0';
+			if (cArr[i] > 9 || cArr[i] < 0) {
+				// exception for values outside of 1-9, ignoring ',' and '_'
+				if (cArr[i] != '_' && cArr[i] != ',') {
+					throw new NumberFormatException("String entered not an integer");
+				}
 			}
 		}
 		
+		// number digits into stack for easy reverse read
 		Stack<Character> cStack = new Stack<>();
 		for (char c : cArr) {
-			cStack.push(c);
+			if (c != '-' && c != '_' && c != ',') cStack.push(c);
 		}
 		
+		// one's place
 		BigInteger num = BigInteger.valueOf(cStack.pop());
+		// adding values for incrementing places
 		long power = 10;
 		for (int i = 0; i > -1; i++) {
 			if(cStack.empty()) break;
 			num = num.add(BigInteger.valueOf(cStack.pop() * power));
 			power *= 10;
 		}
-		int retVal = 0;
 		
+		// back from bigint to check if number is too large
+		// and flipping to negative if flagged
+		int retVal = 0;
 		try {
 			retVal = num.intValueExact();
+			if (isNegative) retVal *= -1;
 		}
 		catch (ArithmeticException e) {
-			throw new NumberFormatException();
+			throw new NumberFormatException("Entered integer too large");
 		}
 		
 		return retVal;
