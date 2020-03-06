afe5ed0f9c0c01b451799a5f3fa4bee0573a4cf5
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2020-03-05 21:00:36 -0500

    racket ctci 1.1, naive O(n^2) solution, trying out nested loops with iterative recursive process

--- scheme/cracking/Ch1/Question_1.1.rkt
+++ scheme/cracking/Ch1/Question_1.1.rkt
@@ -1,15 +1,13 @@
-;public boolean isUnique(String s) {
-;		boolean unique = true;
-;		for (int i = 0; i < s.length(); i++) {
-;			char a = s.charAt(i);
-;			for (int j = i + 1 ; j < s.length(); j++) {
-;				char b = s.charAt(j);
-;				if (a == b) {
-;					unique = false;
-;					break;
-;				}
-;			}
-;		}
+; Cracking the Coding Interview, chapter 1, question 1
 
 ; trying simple 0(n^2) solution to try out racket and ctci
-(define (unique? str) (display str))
+(define (unique? str) 
+  (define (unique?-recur str ind1 ind2)
+      (cond  ((and (equal? (string-ref str ind1) (string-ref str ind2)) (not (= ind1 ind2))) #f)
+              ((< ind2 (- (string-length str) 1)) (unique?-recur str ind1 (+ ind2 1)))
+              ((and (>= ind2 (- (string-length str) 1)) (< ind1 (- (string-length str) 1))) (unique?-recur str (+ ind1 1) 0))
+              ((and (>= ind2 (- (string-length str) 1)) (>= ind1 (- (string-length str) 1))) #t)))
+  (unique?-recur str 0 0))
+
+(unique? "abcdefg")
+(unique? "abcdecg")
