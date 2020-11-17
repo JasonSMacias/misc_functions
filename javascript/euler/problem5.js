e1213781277c4cbdb1e32e6bface2f04cde8b9b1
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2020-11-17 18:31:11 -0500

    Finished prime factor solution to euler javascript problem 5

--- javascript/euler/problem5.js
+++ javascript/euler/problem5.js
@@ -15,40 +15,41 @@ const getSmallestDivisibleBy1to20 = () => {
 }
 
 const getSmallestDivisibleBy1to20UsingPrimeFactors = () => {
-    const freqMap = {};
+    const freqMap = new Map();
     
     for (let i = 2; i <= 20; i++) {
         let count2s = 0;
-        while (!(i / 2)) {
+        let n = i;
+        while (n % 2 == 0) {
             count2s++;
-            i /= 2;
-            console.log(i/2);
+            n /= 2;
         }
-        if (!freqMap["2"] || freqMap["2"] < count2s) {
-            freqMap["2"] = count2s;
+        if (!freqMap.has(2) || freqMap.get(2) < count2s) {
+            freqMap.set(2, count2s);
         }
-        for (let j = 3; j <= Math.sqrt(i); j += 2) {
-            countXs = 0
-            while (!(i / j)) {
-                countXs++;
-                i /= j;
-                
-            }
-            if (!freqMap[j.toString()] || freqMap[j.toString()] < countXs) {
-                freqMap[j.toString()] = countXs;
-            }
-            if(i > 1) {
-                freqMap[i.toString()] = !freqMap[i.toString()] ? freqMap[i.toString()] + 1 : 1;
-            }
+        console.log(i + " -- " + n);
+        for (let x = 3; x <= Math.sqrt(n); x += 2) {
+          let countXs = 0;
+          while (n % x == 0) {
+            countXs++;
+            n /= x;        
+          }
+          if (!freqMap.has(x) == true || freqMap.get(x) < countXs) {
+            console.log("Setting--->" + x + " Countxs: " + countXs)
+            freqMap.set(x, countXs);
+          }
         }
+        if(n > 1) {
+          freqMap.set(n, freqMap.has(n) ? freqMap.get(n) : 1);
+        }
+        console.log(freqMap);
     }
     let retVal = 1;
-    for(let x in freqMap) {
-
+    for(let x of freqMap) {
+      retVal *= Math.pow(x[0], x[1])
     }
     return retVal;
 }
 
-
-
 console.log(getSmallestDivisibleBy1to20UsingPrimeFactors());
+// console.log(getSmallestDivisibleBy1to20());
