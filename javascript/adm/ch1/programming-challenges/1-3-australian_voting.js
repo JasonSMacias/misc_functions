b82b1dfd9f316aee8ae116f5fe30b3386d0956b7
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2022-01-30 21:23:38 -0500

    ch1 australian voting, not getting right outputs for some cases

--- javascript/adm/ch1/programming-challenges/1-3-australian_voting.js
+++ javascript/adm/ch1/programming-challenges/1-3-australian_voting.js
@@ -1,6 +1,74 @@
 const fs = require('fs');
 const readline = require('readline');
+const { getSystemErrorMap } = require('util');
 
+const vote = (election) => {
+    election.isTied = false;
+    for(let i = 0; i < election.candidates.length; i++) {
+        election.currentCandidates.set(i, election.candidates[i]);
+    }
+    return voteHelper(election);
+}
+const voteHelper = (election) => {
+    if(election.currentCandidates.size <= 1 || election.isTied) {
+        let returnString = "";
+        for(candidateObject of election.currentCandidates.values()) {
+            returnString += candidateObject.name + "\n";
+        } 
+        returnString = returnString.trim();
+        return returnString;
+    }
+    for(let candidateObject of election.currentCandidates.values()) {
+        candidateObject.votes = 0;
+    }
+    for(let ballot of election.ballots){
+        // loop through individual ballot(split string and parseint) and add votes to each candidate still in it
+        for(let i = 0; i < 5; i += 2){
+            const candidate = election.currentCandidates.get(parseInt(ballot[i] - 1));
+            if (candidate == null) continue;
+            candidate.votes++;
+            break;
+        }
+    }
+    // loop through current candidates and find those with least.  Remove lowest if not all tied.
+    let lowestVotes = Infinity;
+    let highestVotes = -1;
+    const candidatesWithLowest = [];
+    for(let cand of election.currentCandidates.values()) {
+        if(cand.votes < lowestVotes) lowestVotes = cand.votes;
+        if(cand.votes > highestVotes) highestVotes = cand.votes;
+    }
+    
+    for(let entry of election.currentCandidates.entries()) {
+        if(entry[1].votes <= lowestVotes) {
+            candidatesWithLowest.push(entry[1]);
+            election.currentCandidates.delete(entry[0]);
+            // console.log(entry[1]);
+        }
+     }
+
+    // If all tied, toggle tied value and recursively call.  Either way, then call recursvively
+    if(election.currentCandidates.size < 1) {
+        election.isTied = true;
+        let i = 0;
+        for(let cand of candidatesWithLowest){
+            election.currentCandidates.set(i, cand);
+            i++;
+        }
+    }
+    // Check if 50% threshhold has been met, if so, remove non-winners
+    if(highestVotes > election.ballots.length /2 && election.currentCandidates.size > 1) {
+        for(let entry of election.currentCandidates.entries()) {
+            if(entry[1].votes != highestVotes) {
+                election.currentCandidates.delete(entry[0]);
+            }
+         }
+        //  console.log("\n******SPECIAL CASE*******");
+        //  console.log(election.currentCandidates);
+        //  console.log("******\n");
+    }
+    return voteHelper(election);
+}
 
 const getReadlineInterface = (filePath) => {
     return readline.createInterface({
@@ -19,7 +87,7 @@ function getInputs(readLineInterface) {
         let candidatesLength = null;
         let readyForVoters = false;
         readLineInterface.on('line', (line) => {
-            console.log("processing line: " + line);
+            // console.log("processing line: " + line);
             if (isBeginning && line.length > 0) {
               numberOfElections = parseInt(line);
             } else if(isBeginning) {
@@ -64,7 +132,7 @@ function getInputs(readLineInterface) {
             }
         });
         readLineInterface.on('close', () => {
-            console.log("file read");
+            console.log("file read\n");
             elections.push(election);
             resolve(elections);
         });
@@ -78,10 +146,14 @@ const rlInterface = getReadlineInterface(inputFilePath);
 const inputSetsPromise = getInputs(rlInterface);
 let elections = null;
 inputSetsPromise.then(response => {
-    console.log("Elections ---v");
-    console.log(response);
-    inputSets = response;
+    // console.log("Elections ---v");
+    // console.log(response);
+    elections = response;
+    for (election of elections) {
+        // console.log(election);
+        // console.log(election.candidates)
+        console.log(vote(election));
+        console.log();
+    }
 })
-// const vote = (voters) => {
-//
-// }
+
