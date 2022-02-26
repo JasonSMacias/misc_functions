1f5cf03df435324bb3684bafeb8c801ab7f1af07
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2022-02-26 16:22:17 -0500

    added java Miscellaneous data structure merge sort

--- java/Miscellaneous/src/data_structures_and_standard_algorithms/Driver.java
+++ java/Miscellaneous/src/data_structures_and_standard_algorithms/Driver.java
@@ -4,12 +4,42 @@ import java.util.Arrays;
 
 public class Driver {
 	static QuickSort qs = new QuickSort();
+	static MergeSort ms = new MergeSort();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] testArr1 = {4, 18, 47, 2, 99, 98, 12, -2, 84};
+		int[] testArr2 = {3, 2, 1};
+		int[] testArr3 = {6};
+		int[] testArr4 = new int[0];
+		int[] testArr5 = null;
 		System.out.println(Arrays.toString(testArr1));
-		qs.sort(testArr1);
+//		qs.sort(testArr1);
+		ms.sort(testArr1);
 		System.out.println(Arrays.toString(testArr1));
+		
+		System.out.println(System.lineSeparator() + "------------" + System.lineSeparator());
+		
+		System.out.println(Arrays.toString(testArr2));
+		ms.sort(testArr2);
+		System.out.println(Arrays.toString(testArr2));
+		
+		System.out.println(System.lineSeparator() + "------------" + System.lineSeparator());
+		
+		System.out.println(Arrays.toString(testArr3));
+		ms.sort(testArr3);
+		System.out.println(Arrays.toString(testArr3));
+		
+		System.out.println(System.lineSeparator() + "------------" + System.lineSeparator());
+		
+		System.out.println(Arrays.toString(testArr4));
+		ms.sort(testArr4);
+		System.out.println(Arrays.toString(testArr4));
+		
+		System.out.println(System.lineSeparator() + "------------" + System.lineSeparator());
+		
+		System.out.println(Arrays.toString(testArr5));
+		ms.sort(testArr5);
+		System.out.println(Arrays.toString(testArr5));
 	}
 
 }
--- java/Miscellaneous/src/data_structures_and_standard_algorithms/MergeSort.java
+++ java/Miscellaneous/src/data_structures_and_standard_algorithms/MergeSort.java
@@ -1,5 +1,37 @@
 package data_structures_and_standard_algorithms;
 
-public class MergeSort {
+import java.util.Arrays;
 
+public class MergeSort {
+	public void sort(int[] arr) {
+		if(arr == null || arr.length < 2) return;
+		int mid = arr.length / 2;
+		int[] left = Arrays.copyOf(arr, mid);
+		int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+		sort(left);
+		sort(right);
+		merge(arr, left, right);
+	}
+	private void merge(int[] arr, int[] left, int[] right) {
+		int arrPointer = 0;
+		int lPointer = 0;
+		int rPointer = 0;
+		while (lPointer < left.length && rPointer < right.length) {
+			int lVal = left[lPointer];
+			int rVal = right[rPointer];
+			if(lVal <= rVal) {
+				arr[arrPointer] = lVal;
+				lPointer++;
+			} else {
+				arr[arrPointer] = rVal;
+				rPointer++;
+			}
+			arrPointer++;
+		}
+		if(lPointer < left.length) {
+			System.arraycopy(left, lPointer, arr, arrPointer, left.length - lPointer);
+		} else {
+			System.arraycopy(right, rPointer, arr, arrPointer, right.length - rPointer);
+		}
+	}
 }
